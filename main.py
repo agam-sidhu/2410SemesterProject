b26198f279529727df7272c379ee8417ac41a05f
yolosolodolo <agamrich@gmail.com>
2023-11-06 19:23:35 -0800

    removed unnecessary comments

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ faithful = pd.read_csv('oldfaithful.csv')
 time_between_eruptions=faithful['Eruption']
 length_of_eruptions=faithful['Waiting']
 sns.scatterplot(data = faithful, x = time_between_eruptions, y = length_of_eruptions)
-#this is my test commit
-#test commit
+
 plt.xlabel("Time Between Eruptions (minutes)")
 plt.ylabel("Length of Eruption (minutes)")
 plt.title("Scatter Plot: Time Between Eruptions vs. Length of Eruption")
