fa6b33c151ea6bc0deca888ba83df7f5b43dc2e2
wfgarcia <wfgarcia@cpp.edu>
2023-11-06 19:04:50 -0800

    added scatter plot

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import seaborn as sns
 import matplotlib.plyplot as plt
 
 faithful = pd.read_csv('oldfaithful.csv')
+
+
+sns.scatterplot(data = faithful, x = 'time between eruptions', y = 'length of eruptions')
 #this is my test commit
 #test commit
